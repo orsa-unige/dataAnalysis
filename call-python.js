de8eafefa5fcfe34442f0bf057ca4de065f20be8
Davide Ricci <davide.ricci@oapd.inaf.it>
2018-03-02 12:34:31 +0100

    devel

--- call-python.js
+++ call-python.js
@@ -1,20 +1,34 @@
+var glob = require("glob");
+
 var PythonShell = require('python-shell');
 var pyshell = new PythonShell('./gaussian2dfit-davide.py', {pythonPath : '/usr/bin/python3'});
 
-const data = { filename:"/home/indy/desktop/sim/v-vega/NTT16_5.fits",  x:53, y:46, box:30 };
 
-pyshell.send(JSON.stringify(data), {mode:'json'},{});//the problem function
+// options is optional
+var fitlist=glob.sync("/home/indy/desktop/sim/v-vega/NTT15*1.fits");
 
-pyshell.on('message', function (message) {
-    console.log("asd")
-    console.log(message)
+var outjson=[];
+
+fitlist.forEach(function(item, index){
+    var data = { filename:item,  x:48, y:47, box:30 };
+//    var data = { filename:"/home/indy/desktop/sim/v-vega/NTT15_11.fits",  x:48, y:47, box:30 };
     
-});
+    pyshell.send(JSON.stringify(data), {mode:'json'});//the problem function
+
+    pyshell.on('message', function (message) {
+        outjson.push(message);
+       
+    });
+    
+ });
 
 // end the input stream and allow the process to exit
 pyshell.end(function (err) {
     if (err){
-    console.log('error:');
+        console.log('error:');
         throw err;
     };
+    
+    console.log(outjson);
+
 });
